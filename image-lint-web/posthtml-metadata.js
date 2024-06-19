f70023160c4f92835ec15421a047b81e254ed0d2
Aaron Spaulding <aaron@sachimp.com>
2024-06-19 15:57:11 -0400

    Add package.json metadata to the HTML file

---
+++ image-lint-web/posthtml-metadata.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+export default (options = {}) => (tree) => {
+	// Accept options and set defaults
+	// options.foo = options.foo || {};
+	//
+
+	const package_json = JSON.parse(fs.readFileSync('../image-lint/package.json'));
+
+	/**
+	 * Process a node replacing slots with content from package.json
+	 *
+	 * @param  {any} node
+	 * @return {any}
+	 */
+	function process_node(node) {
+		if (node.tag === 'package-json-metadata') {
+			return {
+				tag: false,
+				content: `
+<title>${ package_json.name } - ${ package_json.version }</title>
+<meta name="description" content="${ package_json.description }">
+`,
+			};
+		}
+		// console.error(node);
+
+		return node;
+	}
+
+	return tree.walk(process_node);
+};
